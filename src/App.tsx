1e3583c20bcbc0c0a06d62057c50e03bc145f1b0
leonardodefendi <leo_defendi@hotmail.com>
2023-08-17 16:07:03 -0300

    Feat req2

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-<<<<<<< HEAD
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-=======
->>>>>>> f91a36db51b1536dc410090644cb592d16f2012d
 import logo from './logo.svg';
 import './App.css';
+import { Home } from './pages/Home';
 
 function App() {
   return (
     <Routes>
-      <Route />
+      <Route element={ <Home /> } path="/" />
     </Routes>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,31 @@
+import { useEffect, useState } from 'react';
+import { getProductsFromCategoryAndQuery } from '../services/api';
+
 export function Home() {
+  const [show, setShow] = useState(true);
+  const [search, setSearch] = useState('');
+  // useEffect(() => {
+  //   const response = async () => {
+  //     const data = await getProductsFromCategoryAndQuery(search);
+  //     if (data.query !== '') {
+  //       setShow(false);
+  //     }
+  //   };
+  //   response();
+  // }, [search]);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    setSearch(value);
+  };
   return (
     <div>
-      <input type="text" />
+      <input type="text" onChange={ handleChange } value={ search } />
+      {show && (
+        <h2 data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </h2>)}
+
     </div>
   );
 }
