acf23f5c4e46a479848047423838a56e939c9b9d
leonardodefendi <leo_defendi@hotmail.com>
2023-08-17 22:50:57 -0300

    Feat req7

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import { Home } from './pages/Home';
 import Carrinho from './pages/Carrinho';
+import { Description } from './pages/Description';
 
 function App() {
   return (
     <Routes>
       <Route element={ <Home /> } path="/" />
       <Route element={ <Carrinho /> } path="/cart" />
+      <Route element={ <Description /> } path="/description/:id" />
     </Routes>
   );
 }
